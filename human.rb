34fee8429ca8feb620d48e3a905f2a8d0b014570
ynsture <ynsture@gmail.com>
2020-04-09 21:30:32 +0300

    completed human part v1

--- human.rb
+++ human.rb
@@ -1,4 +1,17 @@
 class Human
-
+    attr_reader :mark
+def initialize(mark)
+    @mark=mark
+end
+def get_position
+    puts "Player #{mark.to_s} , enter two numbers represents a position for `row col`"
+    pos = gets.chomp.split(' ').map(&:to_i)
+    raise 'sorry,the position was incorrect' if pos.length != 2
+    pos
+end
     
 end
+
+hum=Human.new(:X)
+hum.get_position
+p hum
