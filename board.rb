1d45be76855aa07ef811258dff6a5e3657364f60
rizikolik <ynsture@gmail.com>
2020-04-09 10:17:13 +0000

    completed winner  methods

--- board.rb
+++ board.rb
@@ -8,28 +8,52 @@ class Board
     end
 
     def valid?(pos)
-        return true if @dimension[pos[0],pos[1]]
+		
+        return true if @dimension[pos[0]][pos[1]]
+		false
     end
     def empty?(pos)
-        return true if @dimension[pos[0],pos[1]]=="_"
+        return true if @dimension[pos[0]][pos[1]]=="_"
+		false
     end
     def place_mark(pos,mark)
-        if valid?(pos)&&empty?(pos)
-        @dimension[pos]=mark
+        if  valid?(pos)
+			if empty?(pos)
+			@dimension[pos[0]][pos[1]]=mark
+			else 
+			raise "position is not empty"
+			end
+			
+    
         else   
             raise "please enter a valid position"
         end
     end
 
     def print
-        print @dimension.each{|k| k}
+        @dimension.each do |k|
+            puts k.join(' ')
+        end
+
     end
 
     def win_row?(mark)
+		@dimension.any?{|k| k.all?mark}
     end
     def win_col?(mark)
+		@dimension.transpose.any?{|k| k.all?mark}
     end
     def win_diagonal?(mark)
+		
+		from_left=(0...@dimension.length).each do |k|
+			@dimension[k][k]==mark
+		end
+		from_right=(0...@dimension.length).each do |k|
+			
+		col =@dimension.length-1-i
+			@dimension[k][col]==mark
+		end
+		from_right || from_left
     end
     def win?(mark)
     end
@@ -40,3 +64,21 @@ class Board
 
  
 end
+
+a=Board.new
+ a.place_mark([1, 2], :x)
+a.place_mark([0, 0], :O)
+a.print
+
+
+
+
+
+
+
+
+
+
+
+
+
